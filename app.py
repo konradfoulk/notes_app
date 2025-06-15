c49982571d438e20573c483ad80bc776eef7b444
konradfoulk <konrad.foulk@gmail.com>
2025-06-15 14:06:56 -0400

    level 2 ext.
    my latest progress so I can look at it while I'm at work

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
+# initialize Flask app and database
 app = Flask(__name__)
+db = SQLAlchemy()
+
 app.config['SECRET_KEY'] = '4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+db.init_app(app)
 
 
+# create database table for users
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+
+
+# define routes and functions for webpage
 @app.route('/')
 def index():
     if 'user' in session:
@@ -19,11 +35,10 @@ def login():
         user = request.form['username']
         session['user'] = user
         return redirect(url_for('index'))
+    elif 'user' in session:
+        return redirect(url_for('index'))
     else:
-        if 'user' in session:
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html')
+        return render_template('login.html')
 
 
 @app.route('/signup')

03bbcd48004711ddcd75f510452f6d8f907ee64e
konradfoulk <konrad.foulk@gmail.com>
2025-06-16 12:46:39 -0400

    level 4
    integrated flask login functionality. web app now logs in, out, and can sign up. need to handle incorrect passwords, username already exists, and form security. next step is integrating flask forms

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# initialize Flask app and database
+# initialize Flask app, database, and flask_login
 app = Flask(__name__)
 db = SQLAlchemy()
 
@@ -11,6 +11,15 @@ app.config['SECRET_KEY'] = '4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 # create database table for users
@@ -22,45 +31,53 @@ class User(db.Model, UserMixin):
 
 # define routes and functions for webpage
 @app.route('/')
+@login_required
 def index():
-    if 'user' in session:
-        user = session['user']
-        return render_template('index.html', content=user)
-    else:
-        return redirect(url_for('login'))
+    return render_template('index.html', content=current_user.username)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        user = request.form['username']
-        session['user'] = user
-        return redirect(url_for('index'))
-    elif 'user' in session:
-        return redirect(url_for('index'))
-    else:
-        return render_template('login.html')
+        # get username and password from form on webpage
+        username = request.form['username']
+        password = request.form['password']
+
+        # query the database for the user under that username
+        # if the hashed passwords match, login the user and return the home page
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+
+        # WHAT IF PASSWORD IS INCORRECT?
+    return render_template('login.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
-        user = request.form['username']
+        # get username and password from form on webpage
+        username = request.form['username']
         password = request.form['password']
 
+        # WHAT IF USERNAME ALREADY EXISTS?
+
+        # create new user in database using hashed password
         hashed_password = generate_password_hash(password)
-        new_user = User(username=user, password=hashed_password)
+        new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        # session['user'] = user
-        return redirect(url_for('index'))
 
+        login_user(new_user)
+        return redirect(url_for('index'))
     return render_template('signup.html')
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.pop('user', None)
+    logout_user()
     return redirect(url_for('login'))
 
 

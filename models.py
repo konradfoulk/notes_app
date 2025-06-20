41bab9752f1022e857f65edc02ffb39ee696d78c
konradfoulk <konrad.foulk@gmail.com>
2025-06-20 13:19:30 -0400

    Note db model
    Added db model for notes

--- models.py
+++ models.py
@@ -10,3 +10,14 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)
+
+    notes = db.relationship('Note')
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Text, default='')
+    last_save = db.Column(db.DateTime, default=db.func.now())
+
+    user = db.relationship('User')

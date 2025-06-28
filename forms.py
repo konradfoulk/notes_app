f8a83e0690d80ab1731b26bbb172f81e7fbe00f3
konradfoulk <konrad.foulk@gmail.com>
2025-06-28 13:50:17 -0400

    Final version
    Login and signup pages are now also styled. This is the finished, final version that will be deployed on PythonAnywhere

--- forms.py
+++ forms.py
@@ -6,17 +6,17 @@ from wtforms.validators import DataRequired, Length
 # define flask forms for CSRF protection
 class LoginForm(FlaskForm):
     username = StringField(validators=[
-                           DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'username'})
+                           DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'Enter Username'})
     password = PasswordField(validators=[
-                             DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'password'})
+                             DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'Enter Password'})
     submit = SubmitField('Log In')
 
 
 class SignupForm(FlaskForm):
     username = StringField(validators=[
-                           DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'username'})
+                           DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'Enter Username'})
     password = PasswordField(validators=[
-                             DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'password'})
+                             DataRequired(), Length(min=3, max=30)], render_kw={'placeholder': 'Enter Password'})
     submit = SubmitField('Sign Up')
 
 

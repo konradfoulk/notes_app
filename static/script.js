29b5ef0ae45909f54b0f7bac56fba5fcd1937c57
konradfoulk <konrad.foulk@gmail.com>
2025-06-29 09:12:02 -0400

    Error handling
    Added error handling. Project is finished, may edit it to add Quill editor later

--- static/script.js
+++ static/script.js
@@ -5,13 +5,13 @@ let editor = document.querySelector('#note-editor');
 
 function getTitle(content) {
     if (!content) {
-        return 'New Note'
-    }
+        return 'New Note';
+    };
 
     const contentContainer = document.createElement('div');
-    contentContainer.innerHTML = content
+    contentContainer.innerHTML = content;
 
-    let title = ''
+    let title = '';
     for (let node of contentContainer.childNodes) {
         if (node.nodeType === Node.TEXT_NODE) {
             const text = node.textContent;
@@ -19,25 +19,25 @@ function getTitle(content) {
                 title = text;
                 break;
             };
-        }
+        };
         if (node.nodeType === Node.ELEMENT_NODE) {
             const text = node.innerText;
             if (text) {
                 title = text;
                 break;
-            }
-        }
+            };
+        };
     };
 
     if (!title) {
-        return 'New Note'
-    }
-
-    if (title.length > 20) {
-        return title.slice(0, 17) + '...'
+        return 'New Note';
     };
 
-    return title
+    if (title.length > 20) {
+        return title.slice(0, 17) + '...';
+    };
+
+    return title;
 };
 
 function createBtn(noteId) {
@@ -49,13 +49,13 @@ function createBtn(noteId) {
     btn.addEventListener('click', e => {
         editor.innerHTML = notes.find(note => note.id == noteId).content;
         if (editor.style.visibility == 'hidden') {
-            editor.style.visibility = 'visible'
-        }
+            editor.style.visibility = 'visible';
+        };
 
         if (document.querySelector('.active')) {
-            document.querySelector('.active').classList.remove('active')
-        }
-        e.target.classList.add('active')
+            document.querySelector('.active').classList.remove('active');
+        };
+        e.target.classList.add('active');
 
         currentNoteId = noteId;
     });
@@ -63,70 +63,86 @@ function createBtn(noteId) {
 };
 
 async function createNote() {
-    const response = await fetch('/notes', { method: 'POST' });
-    const data = await response.json();
+    try {
+        const response = await fetch('/notes', { method: 'POST' });
+        const data = await response.json();
 
-    notes.unshift(data);
-    btn = createBtn(data.id);
-    fileList.insertBefore(btn, fileList.firstChild);
+        notes.unshift(data);
+        btn = createBtn(data.id);
+        fileList.insertBefore(btn, fileList.firstChild);
 
-    btn.click()
+        btn.click();
+    } catch (error) {
+        console.log('Error creating note: ' + error);
+    };
 };
 
 async function loadNotes() {
-    const response = await fetch('/notes');
-    const data = await response.json();
-    notes = data;
+    try {
+        const response = await fetch('/notes');
+        const data = await response.json();
+        notes = data;
 
-    for (let note of notes) {
-        btn = createBtn(note.id);
-        fileList.insertBefore(btn, fileList.firstChild);
-    };
-
-    if (fileList.firstChild) {
-        fileList.firstChild.click()
-    } else {
-        createNote()
-    };
-};
-
-async function saveNote() {
-    const content = editor.innerHTML;
-    const response = await fetch(`/notes/${currentNoteId}`, {
-        method: 'PUT',
-        body: JSON.stringify({ content: content }),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await response.json();
-
-    notes.splice(notes.findIndex(note => note.id == currentNoteId), 1);
-    notes.unshift(data);
-
-    btn = document.querySelector(`#note-${currentNoteId}`)
-    if (document.querySelector('#file-list').firstChild.id != 'note-' + currentNoteId) {
-        btn.remove();
-        newBtn = createBtn(currentNoteId);
-        newBtn.classList.add('active');
-        fileList.insertBefore(newBtn, fileList.firstChild);
-    } else {
-        btn.textContent = getTitle(data.content);
-    };
-};
-
-async function deleteNote() {
-    if (notes.find(note => note.id == currentNoteId)) {
-        const response = await fetch(`/notes/${currentNoteId}`, { method: 'DELETE' })
-        const data = await response.json()
-
-        notes.splice(notes.findIndex(note => note.id == data.id), 1);
-        document.querySelector(`#note-${data.id}`).remove()
+        for (let note of notes) {
+            btn = createBtn(note.id);
+            fileList.insertBefore(btn, fileList.firstChild);
+        };
 
         if (fileList.firstChild) {
             fileList.firstChild.click();
         } else {
-            editor.style.visibility = 'hidden'
+            createNote();
         };
-    }
+    } catch (error) {
+        console.log('Error loading notes: ' + error);
+    };
+};
+
+async function saveNote() {
+    try {
+        const content = editor.innerHTML;
+        const response = await fetch(`/notes/${currentNoteId}`, {
+            method: 'PUT',
+            body: JSON.stringify({ content: content }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await response.json();
+
+        notes.splice(notes.findIndex(note => note.id == currentNoteId), 1);
+        notes.unshift(data);
+
+        btn = document.querySelector(`#note-${currentNoteId}`);
+        if (document.querySelector('#file-list').firstChild.id != 'note-' + currentNoteId) {
+            btn.remove();
+            newBtn = createBtn(currentNoteId);
+            newBtn.classList.add('active');
+            fileList.insertBefore(newBtn, fileList.firstChild);
+        } else {
+            btn.textContent = getTitle(data.content);
+        };
+    } catch (error) {
+        console.log('Error saving note: ' + error);
+    };
+};
+
+async function deleteNote() {
+    try {
+        if (notes.find(note => note.id == currentNoteId)) {
+            const response = await fetch(`/notes/${currentNoteId}`, { method: 'DELETE' });
+            const data = await response.json();
+
+            notes.splice(notes.findIndex(note => note.id == data.id), 1);
+            document.querySelector(`#note-${data.id}`).remove();
+
+            if (fileList.firstChild) {
+                fileList.firstChild.click();
+            } else {
+                editor.style.visibility = 'hidden';
+            };
+        };
+    } catch (error) {
+        console.log('Error deleting notes: ' + error);
+    };
 };
 
 document.querySelector('#new-file-btn').addEventListener('click', createNote);
